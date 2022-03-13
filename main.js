f7b553a4344a9f1d9b02dc8055c0fdccad1b0271
Ashour100 <interinstitute5@gmail.com>
2022-03-13 14:42:58 +0100

    Base code

---
+++ main.js
@@ -0,0 +1,12 @@
+/**
+    Creare un oggetto che rappresenti un giocatore di basket, con le seguenti proprietà:
+    Codice giocatore, Nome, Cognome, Età,  Media punti fatti per partita, Percentuale di successo per tiri da 3 punti
+
+    Generare casualmente le statistiche di gioco, secondo queste regole:
+
+    A - il codice giocatore deve essere formato da 3 lettere maiuscole casuali e 3 cifre casuali
+
+    B - la media punti fatti per partita deve essere compresa tra 0 e 50
+    
+    C - la percentuale di successo per da 3 punti deve essere compresa tra 0 e 100
+ */
